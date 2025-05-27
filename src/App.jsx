d704b58c58406c17dd9f57e74aeae15c0a8d6ba6
Guruprasad3n <gp76652@gmail.com>
2025-05-27 14:53:42 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,25 @@ import { Router, Routes, Route } from "react-router-dom";
 import StatePage from "./Pages/StatePage";
 import IndiaMap from "./components/IndiaMap";
 import Navbar from "./components/Navbar";
-import "./App.css"
+import "./App.css";
 import StateBatchWiseData from "./Pages/StateBatchWiseData";
 import MobilizationGallery from "./Pages/MobilizationGallery";
 
 function App() {
   return (
- <>
-    <Navbar/>
+    <>
+      {/* <Navbar/> */}
       <Routes>
         <Route path="/" element={<IndiaMap />} />
         <Route path="/mobilization-gallery" element={<MobilizationGallery />} />
         <Route path="/state/:stateName" element={<StatePage />} />
-        <Route path="/state/:stateName/batches" element={<StateBatchWiseData />} />
+        <Route
+          path="/state/:stateName/batches"
+          element={<StateBatchWiseData />}
+        />
       </Routes>
- </>
- );
+    </>
+  );
 }
 
 export default App;
