325ee89f45ed0c458b211d7ec3abb8db16b4b8de
Guruprasad3n <guruprasad3n@gmail.com>
2025-07-08 15:13:34 +0530

    API Updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    __API_URL__: JSON.stringify(
-      process.env.NODE_ENV === 'production'
-        ? 'https://anaudip-foundation.onrender.com/'
-        : 'http://localhost:3000'
+    // __API_URL__: JSON.stringify(
+    //   process.env.NODE_ENV === 'production'
+    //     ? 'https://anaudip-foundation.onrender.com/'
+    //     : 'http://localhost:3000'
 
-    )
+    // )
+    __API_URL__: JSON.stringify('https://anaudip-foundation.onrender.com/')
   }
 })
